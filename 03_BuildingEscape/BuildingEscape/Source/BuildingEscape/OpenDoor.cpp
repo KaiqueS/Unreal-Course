7399af9500dcfb6d7ac3693cfc1ee679577c1c3a
Kaíque <KaiqueS@users.noreply.github.com>
2017-10-01 20:39:18 -0300

    Sixth Commit - Open Door Trigger Added

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -3,6 +3,8 @@
 #include "OpenDoor.h"
 #include "BuildingEscape.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -16,15 +18,20 @@ UOpenDoor::UOpenDoor()
 
 
 // Called when the game starts
-void UOpenDoor::BeginPlay()
-{
-	Super::BeginPlay();
+void UOpenDoor::BeginPlay( ){
 
+	Super::BeginPlay( );
+
+	ActorThatOpens = GetWorld( ) -> GetFirstPlayerController( ) -> GetPawn( );
+}
+
+void UOpenDoor::OpenDoor( ){
+	
 	AActor* Owner = GetOwner( );
 
-	FRotator NewRotation = FRotator( 0.0f, 90.0f, 0.0f  );
+	FRotator NewRotation = FRotator( 0.0f, 90.0f, 0.0f );
 
-	Owner-> SetActorRotation( NewRotation );
+	Owner->SetActorRotation( NewRotation );
 }
 
 
@@ -33,6 +40,8 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if( PressurePlate -> IsOverlappingActor( ActorThatOpens ) )
+
+		OpenDoor( );
 }
 
--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -18,6 +18,8 @@ public:
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	void OpenDoor( );
 	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -29,4 +31,6 @@ private:
 
 	UPROPERTY( EditAnywhere )
 	ATriggerVolume* PressurePlate;
+
+	AActor* ActorThatOpens;
 };
